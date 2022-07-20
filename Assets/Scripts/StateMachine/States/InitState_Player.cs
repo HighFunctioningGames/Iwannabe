a5e9f4181aab6f84a873e18e542fe24576847321
Rory Burke <ro.hfg.ry@gmail.com>
2022-07-20 16:50:13 -0500

    StateMachine Set-Up

---
+++ Assets/Scripts/StateMachine/States/InitState_Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+*   State that initalizes the player, top use-case is to instantiate and pool animations, could be helpful for other cases, maybe lol
+*/ 
+public class InitState_Player : IState 
+{    
+    public StateMachine SMac { get; set; }
+
+    public void Enter(StateMachine sm) {
+        SMac = sm;
+        SMac.StateText.text = "init";
+    }
+
+    public void Execute() {
+        this.HandleInput();
+        this.LogicUpdate();
+        this.AnimUpdate();
+        this.PhysicsUpdate();
+        Exit();
+    }
+  
+    public void Exit() {
+        
+    }
+
+#region Execute
+    public void HandleInput() {
+       SMac.ChangeState(new MoveState_OffBoard());
+    }
+    public void LogicUpdate() {
+
+    }
+    public void AnimUpdate() {
+
+    }
+    public void PhysicsUpdate() {
+
+    }
+#endregion
+
+
+}
